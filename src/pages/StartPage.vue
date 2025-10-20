662d804fa86699a72ba33ffed6d8f887f405eede
igor <ikondratiev@vita-soft.ru>
2025-10-20 16:33:08 +0300

    feat: init project

---
+++ src/pages/StartPage.vue
@@ -0,0 +1,32 @@
+<script setup>
+</script>
+
+<template>
+  <div class="page start-page">
+    <header class="start-page__header">Справочник организаций</header>
+    <main class="start-page__main">
+    </main>
+  </div>
+</template>
+
+<style>
+
+.start-page {
+  padding-bottom: calc(var(--base) * 0.9);
+}
+
+
+.start-page__header {
+  font-size: calc(var(--base) * 0.48);
+  font-weight: 700;
+  margin-bottom: calc(var(--base) * 0.32);
+  text-align: center;
+}
+
+.start-page__main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex-grow: 1;
+}
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import StartPage from "@/pages/StartPage.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [{
+    path: '/',
+    name: 'StartPage',
+    component: StartPage
+  }],
+})
+
+export default router
